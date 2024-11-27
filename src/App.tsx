2daca7bd767b578832f4107c558fe6a7bd17414b
Suhas Patnaik <suhaspatnaik111@gmail.com>
2024-11-27 23:00:35 +0530

    Tailwind setup with global styles complete

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <>
+      <h1 className="font-bold underline text-neutral-600">Hello world!</h1>
+      <h2 className="font-bold underline">Hello world!</h2>
+      <h3 className="font-bold underline">Hello world!</h3>
+      <h4 className="font-bold underline">Hello world!</h4>
+      <h5 className="font-bold underline">Hello world!</h5>
+      <p className="font-bold underline">Hello world!</p>
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: "hsl(172, 67%, 45%)",
+        },
+        neutral: {
+          100: "hsl(0, 0%, 100%)",
+          200: "hsl(189, 41%, 97%)",
+          300: "hsl(185, 41%, 84%)",
+          400: "hsl(184, 14%, 56%)",
+          500: "hsl(186, 14%, 43%)",
+          600: "hsl(183, 100%, 15%)",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
