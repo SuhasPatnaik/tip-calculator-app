beb8ad965ac1d91c1af48e96720095a677ad23b7
Suhas Patnaik <suhaspatnaik111@gmail.com>
2024-11-29 22:08:45 +0530

    Components skeletal set up done

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
+import { useState } from "react";
 import dollarIcon from "./assets/images/icon-dollar.svg";
 import personIcon from "./assets/images/icon-person.svg";
 
 export default function App() {
   return (
     <>
-      <div className="mt-12">
+      <div className="mt-12 flex justify-center">
         <Logo />
       </div>
       <div>
         <MiniForm inputLabel="Bill" inputIcon={dollarIcon} />
       </div>
       <div>
+        <SelectTipPercentage />
+      </div>
+      <div>
         <MiniForm inputLabel="Number of People" inputIcon={personIcon} />
+      </div>
+      <div>
+        <TipResults />
       </div>
     </>
   );
@@ -30,9 +37,59 @@ function MiniForm({ inputLabel, inputIcon }) {
           type="text"
           style={{
             backgroundImage: `url(${inputIcon})`,
+            backgroundPosition: "1rem",
           }}
-          className="p-2 bg-no-repeat bg-left bg-center text-right h-16"
+          className="px-4 bg-no-repeat bg-left bg-center text-right h-12"
         />
+      </div>
+    </>
+  );
+}
+
+function SelectTipPercentage() {
+  const [isCustomClicked, setIsCustomClicked] = useState(false);
+
+  const handleCustomClick = () => {};
+
+  return (
+    <>
+      <p>Select Tip %</p>
+      <div id="selectTipSection" className="grid grid-cols-2  gap-4">
+        <button className="bg-neutral-600">5%</button>
+        <button className="bg-neutral-600">10%</button>
+        <button className="bg-neutral-600">15%</button>
+        <button className="bg-neutral-600">25%</button>
+        <button className="bg-neutral-600">50%</button>
+        {isCustomClicked ? (
+          <input type="text" />
+        ) : (
+          <button
+            className="bg-neutral-600"
+            onClick={() => setIsCustomClicked(!isCustomClicked)}
+          >
+            Custom
+          </button>
+        )}
+      </div>
+    </>
+  );
+}
+
+function TipResults() {
+  return (
+    <>
+      <div className="bg-neutral-600 grid grid-rows-3">
+        <div className="grid grid-rows-2 grid-cols-2">
+          <p>Tip Amount</p>
+          <p className="row-span-2 text-[2rem] justify-self-end">$4.27</p>
+          <p>/ person</p>
+        </div>
+        <div className="grid grid-rows-2 grid-cols-2">
+          <p>Total</p>
+          <p className="row-span-2 text-[2rem] justify-self-end">$32.79</p>
+          <p>/ person</p>
+        </div>
+        <button className="bg-primary-100">RESET</button>
       </div>
     </>
   );
