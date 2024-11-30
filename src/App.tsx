3e82b2e1b81bd7d339901f1feed7b6a225d74fa6
Suhas Patnaik <suhaspatnaik111@gmail.com>
2024-11-30 22:35:34 +0530

    Tip calculation logic added

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,40 @@ import dollarIcon from "./assets/images/icon-dollar.svg";
 import personIcon from "./assets/images/icon-person.svg";
 
 export default function App() {
+  const [billAmount, setBillAmount] = useState(100);
+  const [numberOfPeople, setNumberOfPeople] = useState(5);
+  const [tipPercentage, setTipPercentage] = useState(0.15);
+
   return (
     <>
       <div className="mt-12 flex justify-center">
         <Logo />
       </div>
       <div>
-        <MiniForm inputLabel="Bill" inputIcon={dollarIcon} />
+        <MiniForm
+          inputLabel="Bill"
+          inputIcon={dollarIcon}
+          inputValue={billAmount}
+          handleChange={(value) => setBillAmount(value)}
+        />
       </div>
       <div>
         <SelectTipPercentage />
       </div>
       <div>
-        <MiniForm inputLabel="Number of People" inputIcon={personIcon} />
+        <MiniForm
+          inputLabel="Number of People"
+          inputIcon={personIcon}
+          inputValue={numberOfPeople}
+          handleChange={(value) => setNumberOfPeople(value)}
+        />
       </div>
       <div>
-        <TipResults />
+        <TipResults
+          billValue={billAmount}
+          headcount={numberOfPeople}
+          tipPercent={tipPercentage}
+        />
       </div>
     </>
   );
@@ -28,13 +46,15 @@ function Logo() {
   return <img src="/logo.svg" alt="Splitter App Logo" />;
 }
 
-function MiniForm({ inputLabel, inputIcon }) {
+function MiniForm({ inputLabel, inputIcon, inputValue, handleChange }) {
   return (
     <>
       <div className="flex flex-col">
         <label>{inputLabel}</label>
         <input
           type="text"
+          value={inputValue}
+          onChange={(e) => handleChange(e.target.value)}
           style={{
             backgroundImage: `url(${inputIcon})`,
             backgroundPosition: "1rem",
@@ -48,8 +68,7 @@ function MiniForm({ inputLabel, inputIcon }) {
 
 function SelectTipPercentage() {
   const [isCustomClicked, setIsCustomClicked] = useState(false);
-
-  const handleCustomClick = () => {};
+  const [customTip, setCustomTip] = useState();
 
   return (
     <>
@@ -61,7 +80,12 @@ function SelectTipPercentage() {
         <button className="bg-neutral-600">25%</button>
         <button className="bg-neutral-600">50%</button>
         {isCustomClicked ? (
-          <input type="text" />
+          <input
+            type="text"
+            className="text-right"
+            value={customTip}
+            onChange={(e) => setCustomTip(Number(e.target.value))}
+          />
         ) : (
           <button
             className="bg-neutral-600"
@@ -75,18 +99,23 @@ function SelectTipPercentage() {
   );
 }
 
-function TipResults() {
+function TipResults({ billValue, headcount, tipPercent }) {
+  const totalTip = (billValue * tipPercent).toFixed(2);
+  const tipPerHead = (totalTip / headcount).toFixed(2);
+
   return (
     <>
       <div className="bg-neutral-600 grid grid-rows-3">
         <div className="grid grid-rows-2 grid-cols-2">
           <p>Tip Amount</p>
-          <p className="row-span-2 text-[2rem] justify-self-end">$4.27</p>
+          <p className="row-span-2 text-[2rem] justify-self-end">
+            ${tipPerHead}
+          </p>
           <p>/ person</p>
         </div>
         <div className="grid grid-rows-2 grid-cols-2">
           <p>Total</p>
-          <p className="row-span-2 text-[2rem] justify-self-end">$32.79</p>
+          <p className="row-span-2 text-[2rem] justify-self-end">${totalTip}</p>
           <p>/ person</p>
         </div>
         <button className="bg-primary-100">RESET</button>
